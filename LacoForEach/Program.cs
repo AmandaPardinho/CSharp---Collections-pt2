39bc9cf2d42e0ccfc93695903a781c7b0d853a49
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-23 16:16:06 -0300

    Explicando o funcionamento do laço foreach

---
+++ LacoForEach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacoForEach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var meses = new List<string>
+            {
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro"
+            };
+
+            foreach (var mes in meses) 
+            {
+                Console.WriteLine(mes);
+            }
+            Console.WriteLine();
+        }
+    }
+}
